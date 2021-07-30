fd6ce2b33829b4f862e1450c2efccd65e8408fd7
Xiaohai Tong <neinstein@bupt.edu.cn>
2021-07-30 17:01:19 +0800

    Update actor.py

--- model/actor.py
+++ model/actor.py
@@ -1,68 +1,73 @@
-import tensorflow as tf
-import numpy as np
-import tflearn
+import torch
+import torch.nn as nn
+from policynet import policyNet
+from copy import deepcopy
 
-class ActorNetwork(object):
-    """
+class ActorNetwork(nn.Module):
+"""
     action network
     use the state
     sample the action
-    """
-    def __init__(self,sess,args):
-        self.global_step=tf.Variable(0,trainable=False,name="ActorStep")
-        self.sess=sess
-        self.dim=args.dim
-        self.lr=args.lr
-        self.optimizer=tf.train.AdamOptimizer(self.lr)
-        self.num_other_variables=len(tf.trainable_variables())
-        self.grained=3418
-        self.L2regular=0.00001
-        self.keep_prob=tf.placeholder(tf.float32,name="keepprob")
-        print("ActorNetwork Done!")
+"""
+    def __init__(self,args):
+        super(ActorNetwork,self).__init__()
+        self.target_policy = policyNet(args)
+        self.active_policy = policyNet(args)
+        self.tau = args.tau
 
-        #actor network(updating)
-        self.input_l,self.input_d,self.scaled_out=self.create_actor_network()
-        self.network_params=tf.trainable_variables()[self.num_other_variables:]
+    def get_target_output(self,h,x,scope):
+        if scope == "target":
+            out = self.target_policy(h,x)
+        if scope == "active":
+            out = self.active_policy(h,x)
+        return out
 
-        #actor network(delayed updating)
-        self.target_input_l,self.target_input_d,self.target_scaled_out=self.create_actor_network()
-        self.target_network_params=tf.trainable_variables()[self.num_other_variables+len(self.network_params):]
+    def get_target_logOutput(self,h,x):
+        out = self.target_policy(h,x)
+        logOut = torch.log(out)
+        return logOut
 
-        # delayed updating actor network
-        self.update_target_network_params = [self.target_network_params[i].assign(
-            tf.multiply(self.network_params[i], self.tau) +
-            tf.multiply(self.target_network_params[i], 1 - self.tau))
-            for i in range(len(self.target_network_params))]
+    def get_gradient(self,h,x,reward,scope):
+        if scope == "target":
+            out = self.target_policy(h,x)
+            logout = torch.log(out).view(-1)
+            index = reward.index(0)
+            index = (index+1)%2
+            #print(out,reward,index,logout[index].view(-1),logout)
+            #print(logout[index].view(-1)
+            grad = torch.autograd.grad(logout[index].view(-1),self.target_policy.parameters())
+            #print(grad[0].size(),grad[1].size(),grad[2].size())
+            #print(grad[0],grad[1],grad[2])
+            grad[0].data = grad[0].data * reward[index]
+            grad[1].data = grad[1].data * reward[index]
+            grad[2].data = grad[2].data * reward[index]
+            #print(grad[0],grad[1],grad[2])
+            return grad
+        if scope == "active":
+            out = self.active_policy(h,x)
+        return out
 
-        self.assign_active_network_params = [self.network_params[i].assign(self.target_network_params[i]) for i in
-                                             range(len(self.network_params))]
+    def assign_active_network_gradients(self,grad1,grad2,grad3):
+        params = [grad1,grad2,grad3]
+        i=0
+        for name,x in self.active_policy.named_parameters():
+            x.grad = deepcopy(params[i])
+            i+= 1
 
-        # gradient provided by critic network
-        self.action_gradient = tf.placeholder(tf.float32, [2])
-        self.log_target_scaled_out = tf.log(self.target_scaled_out)
+    def update_target_network(self):
+        params = []
+        for name,x in self.active_policy.named_parameters():
+            params.append(x)
+        i = 0
+        for name,x in self.target_policy.named_parameters():
+            x.data = deepcopy(params[i].data * (self.tau) + x.data * (1-self.tau))
+            i+=1
 
-        self.actor_gradients = tf.gradients(self.log_target_scaled_out, self.target_network_params,
-                                            self.action_gradient)
-        print(self.actor_gradients)
-
-        self.grads = [tf.placeholder(tf.float32, [600, 1]),
-                      tf.placeholder(tf.float32, [1, ]),
-                      tf.placeholder(tf.float32, [300, 1])]
-
-        self.optimize = self.optimizer.apply_gradients(zip(self.grads, self.network_params[:-1]),
-                                                       global_step=self.global_step)
-
-
-    def create_actor_network(self):
-        input_l=tf.placeholder(tf.float32,shape=[1,self.dim*2])
-        input_d=tf.placeholder(tf.float32,shape=[1,self.dim])
-
-        t1=tflearn.fully_connected(input_l,1)
-        t2=tflearn.fully_connected(input_d,1)
-        scaled_out=tflearn.activation(tf.matmul(input_l,t1.W)+tf.matmul(input_d,t2.W)+t1.b,activation='sigmoid')
-
-        s_out=tf.clip_by_value(scaled_out[0][0],1e-5,1 - 1e-5)
-        scaled_out=tf.stack([1.0-s_out,s_out])
-        return input_l,input_d,scaled_out
-
-    
+    def assign_active_network(self):
+        params = []
+        for name,x in self.target_policy.named_parameters():
+            params.append(x)
+        i = 0
+        for name,x in self.active_policy.named_parameters():
+            x.data = deepcopy(params[i].data)
+            i+=1
